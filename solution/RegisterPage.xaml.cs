51ee1e0cf7bc8ea341f94eae547b1b63d5d1f82d
sammaadaniel30 <danielsamac@gmail.com>
2025-05-14 19:50:37 +0200

    Made the Register Page

---
+++ solution/RegisterPage.xaml.cs
@@ -0,0 +1,15 @@
+namespace practical_work_ii_oop;
+
+public partial class RegisterPage : ContentPage
+{
+    public RegisterPage()
+    {
+        InitializeComponent();
+    }
+
+    // Register button 
+    private void OnRegisterClicked(object sender, EventArgs e)
+    {
+        
+    }
+}
