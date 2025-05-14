cc234ea19ef15ee05703a230264955ac2b80a6bb
sammaadaniel30 <danielsamac@gmail.com>
2025-05-14 19:37:25 +0200

    Made changes so that the program starts in the LoginPage and also created the login page

---
+++ solution/LoginPage.xaml.cs
@@ -0,0 +1,36 @@
+using System;
+using Microsoft.Maui.Controls;
+
+namespace practical_work_ii_oop;
+
+public partial class LoginPage : ContentPage
+{
+    public LoginPage()
+    {
+        InitializeComponent();
+    }
+
+    // Register page
+    private void OnRegisterTapped(object sender, EventArgs e)
+    {
+        
+    }
+
+    // Forgot password link
+    private void OnForgotPasswordTapped(object sender, EventArgs e)
+    {
+        
+    }
+
+    // Sign In button
+    private void OnSignInClicked(object sender, EventArgs e)
+    {
+
+    }
+
+    // Exit the program
+    private void OnExitTapped(object sender, EventArgs e)
+    {
+        
+    }
+}
