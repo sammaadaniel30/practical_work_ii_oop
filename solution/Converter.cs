01db6573a05e427d42f33ce866c78820d2b4d09f
sammaadaniel30 <danielsamac@gmail.com>
2025-05-18 16:42:09 +0200

    Added changes for allowing amount of bits

--- solution/Converter.cs
+++ solution/Converter.cs
@@ -4,27 +4,22 @@ using System.Diagnostics.Contracts;
 
 namespace OOP 
 {
-
     public class Converter
     {
         private List<Conversion> operations; 
 
-
-        
         public Converter()
         {
             this.operations = new List<Conversion>(); 
 
             this.operations.Add(new DecimalToBinary("Binary", "Decimal to Binary"));
-            this.operations.Add(new DecimalToOctal("Binary", "Decimal to Octal"));
+            this.operations.Add(new DecimalToOctal("Octal", "Decimal to Octal"));
             this.operations.Add(new DecimalToHexadecimal("Hexadecimal", "Decimal To Hexadecimal"));
             this.operations.Add(new DecimalToTwoComplement("TwoComplement", "Decimal to Binary(TwoComplement)"));
             this.operations.Add(new BinaryToDecimal("Decimal", "Binary to Decimal")); 
             this.operations.Add(new TwoComplementToDecimal("Decimal", "Binary (TwoComplement) to Decimal"));
             this.operations.Add(new OctalToDecimal("Decimal", "Octal to Decimal")); 
             this.operations.Add(new HexadecimalToDecimal("Decimal", "Hexadecimal to Decimal")); 
-            
-            
         }
 
         public int Exit()
@@ -36,15 +31,14 @@ namespace OOP
         {
             return this.operations.Count(); 
         }
-
+        
         public int PrintOperations()
         {
             Console.WriteLine("-------------------");
-            
+
             for (int i = 1; i <= this.operations.Count(); i++)
             {
                 Console.WriteLine($"{i}. {this.operations[i-1].GetDefinition()} ");
-                
             } 
             Console.WriteLine($"{this.Exit()}. Exit"); 
 
@@ -56,36 +50,31 @@ namespace OOP
             }
 
             int option = int.Parse(tmp);
-
             return (option < 1 || option > this.GetNumberOperations()) ? this.Exit() + 1: option; 
-
-           
-
         }
-        
-        
 
-        public string PerformConversion(int op, string input)
+        // We update the signature to let the pass of the bits needed for certain operations
+        // Default value for bits is 0
+        public string PerformConversion(int op, string input, int bits = 0)
         {
-
-            this.operations[op-1].Validate(input); 
+            this.operations[op-1].Validate(input);
 
             if (this.operations[op-1].NeedBitSize())
             {
-                Console.WriteLine("How many bits should I use"); 
-                int bits = Int32.Parse(Console.ReadLine()); 
+                if (bits <= 0)
+                {
+                    throw new ArgumentException("A valid number of bits is required for this operation.");
+                }
 
-                return this.operations[op-1].Change(input, bits); 
-            } 
+                return this.operations[op-1].Change(input, bits);
+            }
 
-            return this.operations[op-1].Change(input); 
-            
+            return this.operations[op-1].Change(input);
         }
 
         public void PrintConversion(int op, string input, string output)
         {
             this.operations[op-1].PrintConversion(op, input, output);
         }
-
     }
 }
